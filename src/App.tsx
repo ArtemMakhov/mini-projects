51129944a4ead54a9f5acc4a3e85f36b460e83b9
Artem Makhov <makhovartem23@gmail.com>
2023-05-26 13:38:27 +0300

    add logic counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(prev => prev + 1);
+  const dincrement = () => setCount(prev => prev - 1);
+  
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={dincrement} className="minus">- Минус</button>
+        <button onClick={increment} className="plus">Плюс +</button>
       </div>
     </div>
   );
